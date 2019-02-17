0e8bf90ce12283fb402fe4af639aa5809a19a11f
tkud04 <32499735+tkud04@users.noreply.github.com>
2019-02-17 07:56:13 -0800

    Add files via upload

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -24,7 +24,7 @@ class Helper implements HelperContract
                    if($type == "view")
                    {
                      Mail::send($view,$data,function($message) use($to,$subject){
-                           $message->from('ceokhalifawali@gmail.com',"Khalifa Wali");
+                           $message->from('ceochriswoods12@gmail.com',"Khalifa C.W. Aabideen");
                            $message->to($to);
                            $message->subject($subject);
                           if(isset($data["has_attachments"]) && $data["has_attachments"] == "yes")
@@ -40,7 +40,7 @@ class Helper implements HelperContract
                    elseif($type == "raw")
                    {
                      Mail::raw($view,$data,function($message) use($to,$subject){
-                            $message->from('ceokhalifawali@gmail.com',"Khalifa Wali");
+                            $message->from('ceochriswoods12@gmail.com',"Khalifa C.W. Aabideen");
                            $message->to($to);
                            $message->subject($subject);
                            if(isset($data["has_attachments"]) && $data["has_attachments"] == "yes")
